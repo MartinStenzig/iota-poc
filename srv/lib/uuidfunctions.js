da192552466f0d73c3407f8be6188d706da7cace
MartinStenzig <martin@stenzig.com>
2026-03-05 14:21:23 -0800

    Addition of documentation

--- srv/lib/uuidfunctions.js
+++ srv/lib/uuidfunctions.js
@@ -1,5 +1,10 @@
 import getUuid from 'uuid-by-string'
 
+/**
+ * Generates a UUID based on the provided string value.
+ * @param {string} value The string value to generate a UUID from.
+ * @returns {string} The generated UUID.
+ */
 export function uuid4String(value) {
     return getUuid(value)
 }
